54c85ee87a708f983f127a939172f56f6a0424ff
Arthur Machado <arthurmachado7@gmail.com>
2020-07-16 17:55:36 -0300

    Substituido jade por handlebars

--- listen-to-app/routes/index.js
+++ listen-to-app/routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Listen.to', condition: true, anyArray: [1, 2, 3, 4, 5] });
 });
+
+
 
 module.exports = router;
