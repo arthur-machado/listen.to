80d0f36810fe094f7dd015d05cb28b189576f671
arthur-machado <arthurmachado7@gmail.com>
2020-08-17 21:19:00 -0300

    Atualizado arquivos

--- listen-to-app/routes/index.js
+++ listen-to-app/routes/index.js
@@ -1,30 +1,28 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var queryString = require('querystring');
-var SpotifyWebApi = require('spotify-web-api-node');
-const { response, use } = require('../app');
-const { userInfo } = require('os');
-
+var queryString = require("querystring");
+var SpotifyWebApi = require("spotify-web-api-node");
+const { response, use } = require("../app");
+const { userInfo } = require("os");
 
 /* home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Listen.to'});
+router.get("/", function (req, res, next) {
+  res.render("index", {
+    title: "Listen.to",
+  });
 });
 
 /* Página de feed */
 
-router.get('/feed', function(req, res, next) {
-  res.render('feed', { title: 'Listen.to' });
+router.get("/feed", function (req, res, next) {
+  res.render("feed", {
+    title: "Listen.to",
+  });
 });
-
 
 /* Spotify login */
-router.get('/login', function(req, res, next) {
-  res.redirect('http://localhost:8888/login') //redireciona para o servidor de autenticação
+router.get("/login", function (req, res, next) {
+  res.redirect("http://localhost:8888/login"); //redireciona para o servidor de autenticação
 });
-
-
-
-
 
 module.exports = router;
